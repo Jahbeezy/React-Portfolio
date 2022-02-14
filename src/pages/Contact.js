5ad4c3e3823d05a5797bbe8f6f7421ecb007c240
Justin Barner <justinrb14@gmail.com>
2022-02-13 23:42:24 -0800

    fixes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Contact(){
     return(
-        <div className='container d-flex justi text-center'>
+        <div className='container d-flex justify-content-center text-center'>
             <div className='contact row'>
                 <h2>Contact at</h2>
                 <div>Email: JustinRB14@gmail.com</div>
